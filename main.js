6df7b41bab9863e2bdffca1c245f94f7575ccfe9
pyka4y9 <80942085+pyka4y9@users.noreply.github.com>
2021-06-21 23:59:52 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var hero = document.getElementById("hero");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  hero.style.display = "none";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    hero.style.display = "block";
+  }
+}
